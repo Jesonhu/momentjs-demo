b803b25da3c50b0f3fa325fafa44574f997e91f8
Jeson <jesonhu_web@163.com>
2019-03-05 15:24:19 +0800

    feat[getList]: 增加获取目录下 .html 的node.js 代码 docs: 增加列表

---
+++ getList.js
@@ -0,0 +1,32 @@
+// ==================================================
+// 获取目录中 `.html` 文件的 Node.js 脚本.
+// 
+// @author Jesonhu(github)
+// @email jesonhu_web@163.com
+// @update 2019/03/05
+// @version v0.1.0
+// ==================================================
+const path = require('path');
+const fs = require('fs');
+
+const fileDisplaySync = (filePath, fileType) => {
+  const files = fs.readdirSync(filePath);
+  const conArr = [];
+  files.forEach((fileName) => {
+    //获取当前文件的绝对路径
+    const filedir = path.join(filePath, fileName);
+
+    const isExpectType = fileName.indexOf(fileType);
+    let itemStr = '';
+    const basicUrl = 'https://jesonhu.github.io/momentjs-demo/'
+    if (isExpectType) {
+      itemStr = `[${fileName}](${basicUrl}${filedir}/${fileName})`;
+      conArr.push(itemStr);
+    }
+  });
+  console.log(conArr);
+}
+
+const filePath = __dirname + '/';
+const fileType = '.html';
+fileDisplaySync('./', fileType);
